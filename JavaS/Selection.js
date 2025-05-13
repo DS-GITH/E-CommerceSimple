0868522c6dbd7ec55154a0d31249dc446d18bf9d
Luizamc08 <david.tecds@gmail.com>
2025-05-13 09:36:09 -0300

    Basic Selectors

--- JavaS/Selection.js
+++ JavaS/Selection.js
@@ -1,1 +1,4 @@
 document.getElementById("temperature").innerHTML = "Mudando Texto de um Elemento por id";
+document.getElementsByClassName("condition").innerHTML = "Mudando Texto de um Elemento pela class";
+document.getElementsByTagName("div").innerHTML = "Mudando Texto de um Elemento pela Tag";
+document.querySelector("div#location").innerHTML = "Mudando Texto de um Elemento bem especifico";
